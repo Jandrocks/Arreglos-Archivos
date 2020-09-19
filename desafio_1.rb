4bfc37253123408c7db418f6d5acec8524ce25b5
jandrocks <alejandro.meza.chacon@gmail.com>
2020-09-18 21:08:47 -0300

    actualizo desafio1

--- desafio_1.rb
+++ desafio_1.rb
@@ -1,9 +1,16 @@
 a= [1, 9 ,2, 10, 3, 7, 4, 6]
 
 b = a.map{|x| x += 1}
-b = a.map{|x| x.to_f}
-b = a.select{|x| x > 5}
-b = a.inject(0){|sum, x| sum + x}
-b = a.select{|x| x < 5}.count
+print "suma 1 = #{b} \n"
 
-print b
+b = a.map{|x| x.to_f}
+print "valores en float = #{b} \n"
+
+b = a.select{|x| x > 5}
+print "descarta menos que 5 = #{b} \n"
+
+b = a.inject(0){|sum, x| sum + x}
+print "suma con inject  = #{b} \n"
+
+b = a.select{|x| x < 5}.count
+print "count menos a 5 = #{b} \n"
