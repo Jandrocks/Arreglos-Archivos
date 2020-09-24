e295c3a371b566da504280f57347add02f76c131
jandrocks <alejandro.meza.chacon@gmail.com>
2020-09-23 23:15:02 -0300

    desafio2 ARREGLOS

---
+++ nota_mas_alta.rb
@@ -0,0 +1,14 @@
+
+nombre = alejandro
+data = [10,7,50,8]
+m = 0
+data.each do |a|
+    if a > m
+        m = a
+    end
+    
+end
+puts m
+
+
+#print data
--- proyecciones.rb
+++ proyecciones.rb
@@ -3,12 +3,21 @@
     
     data = [300070, 50520, 35000, 32810, 47999, 62050, 89100, 21000, 100010, 121900, 99549, 210000]
     
+    s1 = 0
+    s2 = 0
+    c = 1
+    data.each do |a|
+      if c <= 6
+        s1 =+ a
+      else
+        s2 =+ a
+      end 
+        c += 1
+    end
+    test1 = s1 * 1.1
+    test2 = s2 * 1.2
 
-   # data.each_with_index do |element, index|
-             
-    #    puts "#{index}: #{element}"
-
-       puts data.inject(0) {|sum, x | x + sum }
+    File.write('resultados.data', "#{test1}"" ""#{test2}")
     
 
    
